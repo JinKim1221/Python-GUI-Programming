96fabc5e0d8b631647e5b486951ab480e943d6d7
JinKim1221 <sdo7329@gmail.com>
2021-06-17 15:48:59 +0100

    GUI Frame created

---
+++ GUI_Basic/1_frame.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("Meme Creator")
+root.geometry("640x480") # Setting size of the window
+# root.geometry("640x480+300+100") # horizon, vertical, position of the window(+x, +y)
+
+root.resizable(False, False) # x(width), y(height) values cannot be changed
+root.mainloop() # not to make the window close
