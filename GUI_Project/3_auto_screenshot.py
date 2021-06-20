f26bd7a2afcf22f3780d318d376a72cf277118be
JinKim1221 <sdo7329@gmail.com>
2021-06-20 18:46:43 +0100

    auto screenshot advanced if you press 's' then screen shot 'esc' for exit

--- GUI_Project/3_auto_screenshot.py
+++ GUI_Project/3_auto_screenshot.py
@@ -1,9 +1,19 @@
+# import time
+# from PIL import ImageGrab
+# time.sleep(5) # wait for 5 sec for user to get ready
+# for i in range(1, 11) : # save 10 images every two seconds
+#     img = ImageGrab.grab() # get the current screen image
+#     img.save("image{}.png".format(i)) # save as a file(image1.png ~ image10.png)
+#     time.sleep(2)
 import time
+import keyboard
 from PIL import ImageGrab
 
-time.sleep(5) # wait for 5 sec for user to get ready
+def screenshot():
+    current_time = time.strftime("_%d%m%Y_%H%M%S")
+    img = ImageGrab.grab()
+    img.save("image{}.png".format(current_time))
 
-for i in range(1, 11) : # save 10 images every two seconds
-    img = ImageGrab.grab() # get the current screen image
-    img.save("image{}.png".format(i)) # save as a file(image1.png ~ image10.png)
-    time.sleep(2)
+keyboard.add_hotkey("s", screenshot)
+
+keyboard.wait("esc")
